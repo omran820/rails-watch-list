36033abf8704a3a8100561e747f42e692020a5fc
omram rashidy <omramrashidy@gmail.com>
2022-11-18 18:05:11 +0100

    Hello Berlin

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks, only: [:new ,:create, :show]
+  end
+  resources :bookmarks, only: :destroy
+
 end
